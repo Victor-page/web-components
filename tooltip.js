2313a5106f10359d0a0e9a3fd7f80bb9cdbf1d17
Victor Shevchuk <victor.shevchuk.96@gmail.com>
2021-08-10 15:59:44 +0300

    fixed the previous commit with the hooks

--- tooltip.js
+++ tooltip.js
@@ -1,40 +1,108 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Web Components</title>
-    <style>
-      * {
-        box-sizing: border-box;
-      }
+class Tooltip extends HTMLElement {
+  constructor() {
+    super();
+    this._tooltipIcon;
+    this._tooltipVisible = false;
+    this._tooltipText = 'Default text';
+    this.attachShadow({
+      mode: 'open'
+    });
+    this.shadowRoot.innerHTML = `
+      <style>
+        div {
+          font-weight: normal;
+          background-color: black;
+          color: white;
+          position: absolute;
+          top: 1.6rem;
+          left: 0.75rem;
+          z-index: 1;
+          padding: 0.2rem;
+          border-radius: 3px;
+          box-shadow: 1px 1px 6px rgba(0, 0, 0, 0.26);
+        }
 
-      html {
-        --color-primary: #84c8df;
-        font-family: sans-serif;
+        :host(.important) {
+          background: var(--color-primary, #ccc);
+          padding: 0.2rem;
+        }
+
+        :host-context(p) {
+          font-weight: bold;
+        }
+
+        .highlight {
+          background-color: red;
+        }
+
+        ::slotted(.highlight) {
+          border-bottom: 1px dotted red;
+        }
+
+        .icon {
+          background: black;
+          color: white;
+          padding: 0.15rem 0.5rem;
+          text-align: center; 
+          border-radius: 50%;
+        }
+      </style>
+      <slot>Some default</slot>
+      <span class="icon">?</span>
+    `;
+  }
+
+  connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+    this._tooltipIcon = this.shadowRoot.querySelector('span');
+    this._tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    this._tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.style.position = 'relative';
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['text']
+  }
+
+
+  // disconnectedCallback() {
+  //   this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip);
+  //   const test = this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip);
+  // }
+
+  _render() {
+    let tooltipContainer = this.shadowRoot.querySelector('div');
+    if (this._tooltipVisible) {
+      tooltipContainer = document.createElement('div');
+      tooltipContainer.textContent = this._tooltipText;
+      this.shadowRoot.appendChild(tooltipContainer);
+    } else {
+      if (tooltipContainer) {
+        this.shadowRoot.removeChild(tooltipContainer);
       }
-    </style>
-    <script src="tooltip.js" defer></script>
-    <script src="confirm-link.js" defer></script>
-  </head>
-  <body>
-    <p>
-      <udemy-course-tooltip
-        class="important"
-        text="Web Components is a set of standards."
-      >
-        <span class="highlight">Web Components</span>
-      </udemy-course-tooltip>
-      are awesome!
-    </p>
-    <udemy-course-tooltip></udemy-course-tooltip>
-    <a
-      is="udemy-course-confirm-link"
-      href="https://www.google.com"
-      target="_blank"
-    >
-      Google
-    </a>
-  </body>
-</html>
+    }
+  }
+
+  _showTooltip() {
+    this._tooltipVisible = true;
+    this._render();
+  }
+
+  _hideTooltip() {
+    this._tooltipVisible = false;
+    this._render();
+  }
+}
+
+customElements.define('udemy-course-tooltip', Tooltip);
